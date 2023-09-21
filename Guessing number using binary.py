d7cd771c7b9af06e773d8363113383c55d44ed04
Mitun <98702973+zvoidy@users.noreply.github.com>
2023-09-21 22:15:55 +0530

    Add files via upload

---
+++ Guessing number using binary.py
@@ -0,0 +1,26 @@
+print("Guessing Game!")
+print("----------------------------------")
+print("Think of a number between 1 to 100")
+print("----------------------------------")
+
+low = 1
+high = 100
+Try=0
+
+while True:
+    Try+=1
+    mid=(low+high)//2
+    print("Is it ",mid)
+    response=input("Is my guess correct (Y), your number is too high (H), or too low (L)?: ")
+
+    if response=="Y":
+        print()
+        print(f"I guessed the number {mid} in {Try}tries")
+        print("Thank you for playing!")
+        print()
+        break
+    elif response!="Y" and response=="H":
+        low=mid
+    elif response!="Y" and response=="L":
+        high=mid
+        
